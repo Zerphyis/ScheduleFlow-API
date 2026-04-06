f65c8cfad8278df04e6825f9e14fc0e5c3c5666e
Otavio <estudosprogotavio@gmail.com>
2026-04-06 07:58:55 -0300

    TASK05 feat:adicionando repository POJO de ClientRepository

---
+++ src/main/java/dev/zerphyis/schedule/domain/repositories/ClientRepository.java
@@ -0,0 +1,18 @@
+package dev.zerphyis.schedule.domain.repositories;
+
+import dev.zerphyis.schedule.domain.entites.Client;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ClientRepository {
+
+        Client save(Client client);
+
+        Optional<Client> findById(Long id);
+
+        List<Client> findAll();
+
+        void deleteById(Long id);
+    }
+
