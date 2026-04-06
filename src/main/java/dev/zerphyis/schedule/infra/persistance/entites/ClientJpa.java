7ec1f3f9807c3631efaca501f219a107b9b4fb75
Otavio <estudosprogotavio@gmail.com>
2026-04-06 07:48:12 -0300

    TASK03 feat:adicionando entidade Jpa de Client

---
+++ src/main/java/dev/zerphyis/schedule/infra/persistance/entites/ClientJpa.java
@@ -0,0 +1,51 @@
+package dev.zerphyis.schedule.infra.persistance.entites;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ClientJpa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String cpf;
+    private String telefone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
