422ecdc8b21ae3cdb74cb8dc240da7ad999e418d
Otavio <estudosprogotavio@gmail.com>
2026-04-07 09:22:24 -0300

    TASK06 refactor:corrigindo metodos toDomain e toEntity em ClientMapper

--- src/main/java/dev/zerphyis/schedule/infra/mappers/ClientMapper.java
+++ src/main/java/dev/zerphyis/schedule/infra/mappers/ClientMapper.java
@@ -5,26 +5,27 @@ import dev.zerphyis.schedule.infra.persistance.entites.ClientJpa;
 
 public class ClientMapper {
 
-        public static Client toDomain(ClientJpa entity) {
-            if (entity == null) return null;
+    public static Client toDomain(ClientJpa entity) {
+        if (entity == null) return null;
 
-            Client client = new Client();
-            client.setId(entity.getId());
-            client.setNome(entity.getNome());
-            client.setCpf(entity.getCpf());
-            client.setTelefone(entity.getTelefone());
+        Client client = new Client();
+        client.setId(entity.getId());
+        client.setNome(entity.getNome());
+        client.setCpf(entity.getCpf());
+        client.setTelefone(entity.getTelefone());
 
-            return client;
-        }
-
-        public static ClientJpa toEntity(Client domain) {
-            if (domain == null) return null;
-
-            return new ClientJpa(
-                    domain.getId(),
-                    domain.getNome(),
-                    domain.getCpf(),
-                    domain.getTelefone()
-            );
-        }
+        return client;
     }
+
+    public static ClientJpa toEntity(Client domain) {
+        if (domain == null) return null;
+
+        ClientJpa entity = new ClientJpa();
+        entity.setId(domain.getId());
+        entity.setNome(domain.getNome());
+        entity.setCpf(domain.getCpf());
+        entity.setTelefone(domain.getTelefone());
+
+        return entity;
+    }
+}
