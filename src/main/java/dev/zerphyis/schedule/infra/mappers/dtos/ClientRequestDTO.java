f7bf93ec4b1af50b96459ea8ce792065569c75af
Otavio <estudosprogotavio@gmail.com>
2026-04-07 09:22:02 -0300

    TASK06 feat:adicionando dto ClientRequestDTO

---
+++ src/main/java/dev/zerphyis/schedule/infra/mappers/dtos/ClientRequestDTO.java
@@ -0,0 +1,14 @@
+package dev.zerphyis.schedule.infra.mappers.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record ClientRequestDTO(
+        @NotBlank(message = "Nome é obrigatório")
+        String nome,
+
+        @NotBlank(message = "CPF é obrigatório")
+        String cpf,
+
+        @NotBlank(message = "Telefone é obrigatório")
+        String telefone) {
+}
