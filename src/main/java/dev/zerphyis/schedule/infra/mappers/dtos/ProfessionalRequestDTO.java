f57ce5644e4770bdd84f4f90172b3798fb0486ed
Otavio <estudosprogotavio@gmail.com>
2026-04-07 09:20:49 -0300

    TASK06 feat: adicionando ProfessionalRequestDTO

---
+++ src/main/java/dev/zerphyis/schedule/infra/mappers/dtos/ProfessionalRequestDTO.java
@@ -0,0 +1,16 @@
+package dev.zerphyis.schedule.infra.mappers.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record ProfessionalRequestDTO(
+        @NotBlank(message = "Nome é obrigatório")
+        String nome,
+
+        @NotBlank(message = "Especialidade é obrigatória")
+        String especialidade,
+
+        @NotBlank(message = "Email é obrigatório")
+        @Email(message = "Email inválido")
+        String email) {
+}
