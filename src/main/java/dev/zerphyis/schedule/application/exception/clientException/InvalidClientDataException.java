906f59d6d1405cc25bdb274031fc78e699481962
Otavio <estudosprogotavio@gmail.com>
2026-04-08 05:12:21 -0300

    TASK07 feat:adicionando InvalidClientDataException

---
+++ src/main/java/dev/zerphyis/schedule/application/exception/clientException/InvalidClientDataException.java
@@ -0,0 +1,8 @@
+package dev.zerphyis.schedule.application.exception.clientException;
+
+
+public class InvalidClientDataException extends RuntimeException {
+    public InvalidClientDataException(String message) {
+        super(message);
+    }
+}
