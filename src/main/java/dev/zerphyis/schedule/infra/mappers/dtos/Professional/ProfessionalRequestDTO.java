f03f0035776090448e6fe14f59834cde332be549
Otavio <estudosprogotavio@gmail.com>
2026-04-09 02:58:55 -0300

    TASK07 refactor:movendo ProfessionalRequestDTO para novo pacote Professional

--- src/main/java/dev/zerphyis/schedule/infra/mappers/dtos/Professional/ProfessionalRequestDTO.java
+++ src/main/java/dev/zerphyis/schedule/infra/mappers/dtos/Professional/ProfessionalRequestDTO.java
@@ -1,4 +1,4 @@
-package dev.zerphyis.schedule.infra.mappers.dtos;
+package dev.zerphyis.schedule.infra.mappers.dtos.Professional;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
