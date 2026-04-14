7e2df5000b759016034ec80d24edbec714fd2d96
Otavio <estudosprogotavio@gmail.com>
2026-04-14 07:59:07 -0300

    TASK11 feat:Adicionando metodo findAllByProfessionalIdOrderBy em AppointmentRepositoryJpa

--- src/main/java/dev/zerphyis/schedule/infra/persistance/repository/AppointmentRepositoryJpa.java
+++ src/main/java/dev/zerphyis/schedule/infra/persistance/repository/AppointmentRepositoryJpa.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.Repository;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public interface AppointmentRepositoryJpa extends JpaRepository<AppointmentJpa,Long> {
     boolean existsByProfessionalIdAndDateTime(Long professionalId, LocalDateTime dateTime);
+    List<AppointmentJpa> findAllByProfessionalIdOrderByDateTimeAsc(Long professionalId);
 
 }
