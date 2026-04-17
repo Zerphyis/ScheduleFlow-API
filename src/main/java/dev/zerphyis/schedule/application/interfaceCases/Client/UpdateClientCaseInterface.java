c8b89ff2f13178980516c705e10215a6d9c3d379
Otavio <estudosprogotavio@gmail.com>
2026-04-17 11:19:12 -0300

    TASK15 refactor:corrigindo nome DTo em UpdateClientCaseInterface

--- src/main/java/dev/zerphyis/schedule/application/interfaceCases/Client/UpdateClientCaseInterface.java
+++ src/main/java/dev/zerphyis/schedule/application/interfaceCases/Client/UpdateClientCaseInterface.java
@@ -1,8 +1,8 @@
 package dev.zerphyis.schedule.application.interfaceCases.Client;
 
-import dev.zerphyis.schedule.infra.mappers.dtos.Clients.ClientReponseDTO;
+import dev.zerphyis.schedule.infra.mappers.dtos.Clients.ClientResponseDTO;
 import dev.zerphyis.schedule.infra.mappers.dtos.Clients.ClientRequestDTO;
 
 public interface UpdateClientCaseInterface {
-    ClientReponseDTO execute(Long id, ClientRequestDTO dto);
+    ClientResponseDTO execute(Long id, ClientRequestDTO dto);
 }
