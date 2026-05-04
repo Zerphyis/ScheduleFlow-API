643f4cfa2154a59a037be1eb5230433792957c19
Otavio <estudosprogotavio@gmail.com>
2026-05-04 05:25:57 -0300

    TASK16 feat: adicionando interfaceCase CreateClientCaseInterface

--- src/main/java/dev/zerphyis/schedule/application/interfaceCases/Client/CreateClientCaseInterface.java
+++ src/main/java/dev/zerphyis/schedule/application/interfaceCases/Client/CreateClientCaseInterface.java
@@ -1,8 +1,8 @@
 package dev.zerphyis.schedule.application.interfaceCases.Client;
 
-import dev.zerphyis.schedule.infra.mappers.dtos.Clients.ClientReponseDTO;
+import dev.zerphyis.schedule.infra.mappers.dtos.Clients.ClientResponseDTO;
 import dev.zerphyis.schedule.infra.mappers.dtos.Clients.ClientRequestDTO;
 
 public interface CreateClientCaseInterface {
-    ClientReponseDTO execute(ClientRequestDTO dto);
+    ClientResponseDTO execute(ClientRequestDTO dto);
 }
