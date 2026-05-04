e4c0a9193ad35c9408887a8f876862f23a006ce8
Otavio <estudosprogotavio@gmail.com>
2026-05-04 05:36:54 -0300

    TASK17 feat: adicionando Cache em AppointmentService

--- src/main/java/dev/zerphyis/schedule/application/useCases/Appointment/AppointmentService.java
+++ src/main/java/dev/zerphyis/schedule/application/useCases/Appointment/AppointmentService.java
@@ -2,11 +2,12 @@ package dev.zerphyis.schedule.application.useCases.Appointment;
 
 import dev.zerphyis.schedule.infra.mappers.dtos.Appointments.AppointmentResponseDTO;
 import dev.zerphyis.schedule.infra.mappers.dtos.Appointments.AppointmentRequestDTO;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 
 import java.util.List;
 
 public class AppointmentService {
-
 
     private final CreateAppointmentUseCase createAppointmentUseCase;
     private final ListProfessionalScheduleUseCase listProfessionalScheduleUseCase;
@@ -22,14 +23,17 @@ public class AppointmentService {
         this.cancelAppointmentUseCase = cancelAppointmentUseCase;
     }
 
+    @CacheEvict(value = "appointments", key = "#request.professionalId")
     public AppointmentResponseDTO create(AppointmentRequestDTO request) {
         return createAppointmentUseCase.execute(request);
     }
 
+    @Cacheable(value = "appointments", key = "#professionalId")
     public List<AppointmentResponseDTO> listByProfessional(Long professionalId) {
         return listProfessionalScheduleUseCase.execute(professionalId);
     }
 
+    @CacheEvict(value = "appointments", allEntries = true)
     public void cancel(Long appointmentId) {
         cancelAppointmentUseCase.execute(appointmentId);
     }
